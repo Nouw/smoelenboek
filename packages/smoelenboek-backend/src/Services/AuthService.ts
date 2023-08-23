89d029be334f99c6a12cb457a9fd2001af61ba16
Fabio Dijkshoorn <offabio@outlook.com>
2023-08-23 19:00:34 +0200

    Updated deploy and auth typings

--- packages/smoelenboek-backend/src/Services/AuthService.ts
+++ packages/smoelenboek-backend/src/Services/AuthService.ts
@@ -1,26 +1,34 @@
-import { User } from "smoelenboek-types";
+import {User} from "smoelenboek-types";
 import jwt from "jsonwebtoken";
 import moment from "moment";
 
+interface Token {
+  exp: number;
+  id: number;
+  email: string;
+  refresh: boolean;
+  iat: number;
+}
+
 export default class AuthService {
-	encryptionKey = process.env.ENCRYPTION_KEY ?? "secret";
+  encryptionKey = process.env.ENCRYPTION_KEY ?? "secret";
 
-	getTokens(user: User) {
-		const authToken = jwt.sign({
-			id: user.id,
-			email: user.email,
-			refresh: false
-		}, this.encryptionKey, { expiresIn: moment().add(5, "m").unix() });
+  getTokens(user: User) {
+    const authToken = jwt.sign({
+      id: user.id,
+      email: user.email,
+      refresh: false
+    }, this.encryptionKey, {expiresIn: moment().add(5, "m").unix()});
 
-		const refreshToken = jwt.sign({
-			id: user.id,
-			refresh: true
-		}, this.encryptionKey, { expiresIn: moment().add(6, "M").unix() });
+    const refreshToken = jwt.sign({
+      id: user.id,
+      refresh: true
+    }, this.encryptionKey, {expiresIn: moment().add(6, "M").unix()});
 
-		return { authToken, refreshToken };
-	}
+    return {authToken, refreshToken};
+  }
 
-	decodeToken(authToken: string) {
-		return jwt.verify(authToken, this.encryptionKey);
-	}
+  decodeToken(authToken: string): Token {
+    return jwt.verify(authToken, this.encryptionKey) as Token;
+  }
 }
