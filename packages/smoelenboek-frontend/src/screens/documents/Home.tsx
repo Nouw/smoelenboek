fbf007f39d53cde973ca779291665036023326f8
Fabio Dijkshoorn <offabio@outlook.com>
2023-10-27 19:06:33 +0200

    Added notification about photo's not rendering

--- packages/smoelenboek-frontend/src/screens/documents/Home.tsx
+++ packages/smoelenboek-frontend/src/screens/documents/Home.tsx
@@ -1,11 +1,19 @@
 import React from "react";
-import {Card, CardContent, CircularProgress, List, ListItem, ListItemButton, ListItemText} from "@mui/material";
-import {Category} from "../../components/documents/Category";
-import {useNavigate} from "react-router-dom";
-import {useDocumentsCategoriesQuery} from "../../api/endpoints/documents";
+import {
+  Alert,
+  Card,
+  CardContent,
+  CircularProgress,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
+import { Category } from "../../components/documents/Category";
+import { useNavigate } from "react-router-dom";
+import { useDocumentsCategoriesQuery } from "../../api/endpoints/documents";
 
 interface HomeProps {
-
 }
 
 export const Home: React.FC<HomeProps> = () => {
@@ -13,33 +21,41 @@ export const Home: React.FC<HomeProps> = () => {
 
   const { data, isLoading } = useDocumentsCategoriesQuery(false);
 
-  if (isLoading) return <CircularProgress/>
+  if (isLoading) return <CircularProgress />;
 
   const categories = data!.data;
 
   return (
-    <Card>
-      <CardContent>
-        <List>
-          {Object.keys(categories).map((key) => (
-            <>
-              {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
-              {/* @ts-ignore */}
-              <Category name={categories[key].name}>
+    <>
+      <Alert severity="warning" sx={{ mb: 2 }}>
+        Er zijn momenteel problemen met het laden van sommige foto's, dit
+        probleem is bekend bij Oracle en die zijn bezig met een oplossing.
+      </Alert>
+      <Card>
+        <CardContent>
+          <List>
+            {Object.keys(categories).map((key) => (
+              <>
                 {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
                 {/* @ts-ignore */}
-                {categories[key].categories.map((category) => (
-                  <ListItem>
-                    <ListItemButton onClick={() => navigate(`files/${category.id}`)}>
-                      <ListItemText primary={category.name}/>
-                    </ListItemButton>
-                  </ListItem>
-                ))}
-              </Category>
-            </>
-          ))}
-        </List>
-      </CardContent>
-    </Card>
-  )
-}
+                <Category name={categories[key].name}>
+                  {/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */}
+                  {/* @ts-ignore */}
+                  {categories[key].categories.map((category) => (
+                    <ListItem>
+                      <ListItemButton
+                        onClick={() => navigate(`files/${category.id}`)}
+                      >
+                        <ListItemText primary={category.name} />
+                      </ListItemButton>
+                    </ListItem>
+                  ))}
+                </Category>
+              </>
+            ))}
+          </List>
+        </CardContent>
+      </Card>
+    </>
+  );
+};
